ea1fbbd604c0b054479a9df8374c09e779d0cadc
THISISAGOODNAME <yangyj19931231@gmail.com>
2023-04-12 20:24:19 +0800

    Draw texture

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ constexpr auto NES_HEIGHT = 240;
 
 sg_pass_action pass_action{};
 sg_buffer vbuf{};
+sg_buffer ibuf{};
 sg_pipeline pip{};
 sg_bindings bind{};
 
@@ -26,52 +27,84 @@ void init() {
     sg_setup(&desc);
 
     const float vertices[] = {
-            // positions            // colors
-            0.0f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f,
-            0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f, 1.0f,
-            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f
+            // positions     uv
+            -1.0, -1.0, 0.0, 0.0, 1.0,
+            1.0,  -1.0, 0.0, 1.0, 1.0,
+            1.0,  1.0,  0.0, 1.0, 0.0,
+            -1.0, 1.0,  0.0, 0.0, 0.0,
     };
     sg_buffer_desc vb_desc = {};
     vb_desc.data = SG_RANGE(vertices);
     vbuf = sg_make_buffer(&vb_desc);
 
+    const int indices[] = { 0, 1, 2, 0, 2, 3, };
+    sg_buffer_desc ib_desc = {};
+    ib_desc.type = SG_BUFFERTYPE_INDEXBUFFER;
+    ib_desc.data = SG_RANGE(indices);
+    ibuf = sg_make_buffer(&ib_desc);
+
     sg_shader_desc shd_desc = {};
+    shd_desc.attrs[0].name = "position";
+    shd_desc.attrs[1].name = "texcoord0";
     shd_desc.vs.source = R"(
 #version 330
-layout(location=0) in vec4 position;
-layout(location=1) in vec4 color0;
+layout(location=0) in vec3 position;
+layout(location=1) in vec2 texcoord0;
 out vec4 color;
+out vec2 uv;
 void main() {
-  gl_Position = position;
-  color = color0;
+  gl_Position = vec4(position, 1.0f);
+  uv = texcoord0;
+  color = vec4(uv, 0.0f, 1.0f);
 }
 )";
+    shd_desc.fs.images[0].name = "tex";
+    shd_desc.fs.images[0].image_type = SG_IMAGETYPE_2D;
+    shd_desc.fs.images[0].sampler_type = SG_SAMPLERTYPE_FLOAT;
     shd_desc.fs.source = R"(
 #version 330
+uniform sampler2D tex;
 in vec4 color;
+in vec2 uv;
 out vec4 frag_color;
 void main() {
-  frag_color = color;
+  frag_color = texture(tex, uv);
 }
 )";
+    uint32_t pixels[4*4] = {
+            0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
+            0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
+            0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
+            0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
+    };
+    /* NOTE: SLOT_tex is provided by shader code generation */
+    sg_image_desc img_desc = {};
+    img_desc.width = 4;
+    img_desc.height = 4;
+    img_desc.data.subimage[0][0] = SG_RANGE(pixels);
+    img_desc.label = "cube-texture";
+
     sg_shader shd = sg_make_shader(&shd_desc);
 
     sg_pipeline_desc pip_desc = {};
     pip_desc.shader = shd;
     pip_desc.layout.attrs[0].format = SG_VERTEXFORMAT_FLOAT3;
-    pip_desc.layout.attrs[1].format = SG_VERTEXFORMAT_FLOAT4;
+    pip_desc.layout.attrs[1].format = SG_VERTEXFORMAT_FLOAT2;
+    pip_desc.index_type = SG_INDEXTYPE_UINT32;
     pip = sg_make_pipeline(&pip_desc);
 
     bind.vertex_buffers[0] = vbuf;
+    bind.index_buffer = ibuf;
+    bind.fs_images[0] = sg_make_image(&img_desc);
 
-    pass_action.colors[0] = { .action=SG_ACTION_CLEAR, .value={1.0f, 0.0f, 0.0f, 1.0f} };
+    pass_action.colors[0] = { .action=SG_ACTION_CLEAR, .value={0.5f, 0.0f, 0.0f, 1.0f} };
 }
 
 void frame() {
     sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
     sg_apply_pipeline(pip);
     sg_apply_bindings(&bind);
-    sg_draw(0, 3, 1);
+    sg_draw(0, 6, 1);
     sg_end_pass();
     sg_commit();
 }
